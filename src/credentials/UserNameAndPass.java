fbce024309405fbd2c7c76f926920e15d120d0d9
nao-f-lll <111573910+nao-f-lll@users.noreply.github.com>
2023-10-12 12:32:48 +0000

    remove duplicate credentials

--- src/credentials/UserNameAndPass.java
+++ src/credentials/UserNameAndPass.java
@@ -7,15 +7,11 @@ public class UserNameAndPass {
     HashMap<String, String> loginInfo = new HashMap<String, String>();
 
     public UserNameAndPass() {
-
         loginInfo.put("naoufal", "naoufal");
         loginInfo.put("adioual@gmail.com", "ahmed");
         loginInfo.put("ahmadhkary53@gmail.com", "ahmad");
         loginInfo.put("lucasbarru3@gmail.com", "lucas");
-        loginInfo.put("sendoave@gmail.com", "sendoa");
-        loginInfo.put("sendoave@gmail.com", "sendoa");
-       
-        
+        loginInfo.put("sendoave@gmail.com", "sendoa");     
     }
       
     
