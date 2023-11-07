7de75170c0fa83ac3ad49d2d465842b019676c9b
ik_1dw3a <ik_1dw3a@H015110.NET>
2023-11-07 08:05:52 +0100

    remove the commented code and add two images for editing and click to edit

--- src/com/standings/model/CustomCheckBox.java
+++ src/com/standings/model/CustomCheckBox.java
@@ -14,7 +14,7 @@ import javax.swing.JCheckBox;
 public class CustomCheckBox extends JCheckBox {
 
 	private static final long serialVersionUID = -7104751621123160976L;
-	private final int border = 4;
+	
 	private ImageIcon clickToEditIcon;
 	private ImageIcon editorIcon;
 	
@@ -35,29 +35,17 @@ public class CustomCheckBox extends JCheckBox {
 		super.paint(graphics);
 		Graphics2D graphics2d = (Graphics2D) graphics;
 		graphics2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		int yCordinate = (getHeight() - 16 ) / 2;
 		if (isSelected()) {
 			if (isEnabled()) {
-				//graphics2d.setColor(getBackground());
+			
 				
 				this.setIcon(clickToEditIcon);		
 				setToolTipText("Haga click para dejar de editar");
 			} else {
-				//graphics2d.setColor(Color.GREEN);
+				
 			}
-			
-			//graphics2d.fillRoundRect(1, yCordinate, 16, 16, border, border);
-			
-			//int px[] = {4, 8, 14, 12, 8 ,6};
-			//int py[] = {yCordinate + 8, yCordinate + 14, yCordinate + 5, yCordinate + 3, yCordinate + 10, yCordinate + 6};
-			//graphics2d.setColor(Color.WHITE);
-			//graphics2d.fillPolygon(px, py, px.length);
-			
+		
 		} else {
-			//graphics2d.setColor(Color.GRAY);
-			//graphics2d.fillRoundRect(1, yCordinate, 16, 16, border, border);
-			//graphics2d.setColor(Color.WHITE);
-			//graphics2d.fillRoundRect(2, yCordinate + 1, 14, 14, border, border);
 			
 			this.setIcon(editorIcon);
 			setToolTipText("Haga click para editar");
